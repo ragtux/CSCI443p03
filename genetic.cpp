8bc5126180560b8456c5086d3b2dd51b4ad06807
Al-Sharabati <tl3482xg@ahscentos.CAMPUS.STCLOUDSTATE.EDU>
2019-10-27 15:32:32 -0500

    genetic and evocomp compile now

--- genetic.cpp
+++ genetic.cpp
@@ -5,44 +5,35 @@ int main() {
 	startup();
 
 	//Gen size is maximum 1/5 the number of vertices in graph or 32 chromosomes, whichever is smaller. Always more than one.
-	int gen_size = Vertices.size();
-	if (gen_size < 2)
-		gen_size = 2;
-	if (gen_size > 32)
-		gen_size = 32;
+	const int gen_size = VERT_NO;
 
-	Chromosome *old_gen[gen_size];
-	Chromosome *new_gen[gen_size];
+	array<Chromosome *, gen_size> old_gen;
+	array<Chromosome *, (const int) (gen_size*0.9)> new_gen;
 
-	//seed prototype/old generation with chromosomes that only have one vertex active
+	//seed prototype/old generation with chromosomes
 	for (int i = 0; i < gen_size; i++) {
-		auto j = Vertices.begin();
-		std::advance(j, rand() % Vertices.size());
 		old_gen[i] = new Chromosome();
-		old_gen[i]->active[j->first] = true;
-		old_gen[i]->fitness++;
+		iota(old_gen[i]->permutation.begin(), old_gen[i]->permutation.end(), 1);
+		random_shuffle(begin(old_gen), end(old_gen));
+		old_gen[i]->evalFitness();
 	}
 
 	// Generation
 	do {
-		for (int i = 0; i < gen_size; i++) {
-			if (((float) rand()) / RAND_MAX < 0.5) {
+		for (int i = 0; i < gen_size * 0.9; i++) {
+			int p1 = get_parent(old_gen);
+			if (((float) rand()) / RAND_MAX < 0.3) {
 				//select one chrs and mutate;
-				int j = rand() % gen_size;
-				new_gen[i] = old_gen[j]->mutate();
+				new_gen[i] = old_gen[p1]->mutate();
 			} else {
 				//select two parents
-				int p1 = rand() % gen_size, p2;
-				do {
-					p2 = rand() % gen_size;
-				} while (p1 == p2);
+				int p2 = get_parent(old_gen);
 				new_gen[i] = cross(old_gen[p1], old_gen[p2]);
 			}
 		}
 
-		sort(new_gen, new_gen+gen_size, sort_by_fitness);
-
 		//replace a maximum of 1/5 of old_gen with better fit from new_gen
+		/*
 		for (int i = 0, j = gen_size-1; i <= gen_size/5; i++, j--) {
 			if (*(new_gen[j]) > *(old_gen[i])) {
 				delete old_gen[i];
@@ -55,8 +46,9 @@ int main() {
 			if (new_gen[i])
 				delete new_gen[i];
 		}
+		*/
 
-		sort(old_gen, old_gen+gen_size, sort_by_fitness); 
+		sort(begin(old_gen), end(old_gen), sort_by_fitness); 
 
 		cout << "Best fitness is " << old_gen[gen_size-1]->fitness << ". Chrs is " << old_gen[gen_size-1] << endl;
 	} while (old_gen[gen_size-1]->fitness != old_gen[0]->fitness);
