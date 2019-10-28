303e67cb43ee62ef34bfe716535d0692694f663f
bear <bear@kr38.local>
2019-10-27 19:45:03 -0500

    prim-based Heuristic first commit

--- heuristic.cpp
+++ heuristic.cpp
@@ -1,32 +1,115 @@
-//File: heuristic.cpp
-#include "EvoComp.hpp"
+#include <bits/stdc++.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
 
-struct count {
-	int id;
-	int count;
-};
 
-bool sort_by_count (struct count x, struct count y) { return (x.count < y.count);}
+using namespace std; 
 
-int main () {
-	startup();
-	vector<struct count> edge_count;
-	
-	Chromosome *heur = new Chromosome();
-	for (map<int,Vertex>::iterator i = Vertices.begin(); i != Vertices.end(); i++) {
-		struct count j;
-		j.id = i->first;
-		j.count = i->second.neigh.size();
-		edge_count.push_back(j);
+// number of vertices in the graph 
+const int VERT_NO = 50; 
+int weight[VERT_NO][VERT_NO];
+
+void startup()
+{
+	srand(time(NULL));
+
+	FILE *infile = fopen("test.dat", "r");
+	//check for file errs here
+
+	for (int i = 0; i < VERT_NO; i++) 
+	{
+		for (int j = i + 1; j < VERT_NO; j++) 
+		{
+			if (!feof(infile))
+			{
+				fscanf(infile, "%i", &(weight[j][i]));
+				weight[i][j] = weight[j][i];
+			}
+		}
 	}
-	cout << endl;
 
-	sort(edge_count.begin(), edge_count.end(), sort_by_count);
-
-	for (unsigned int i = 0; i < edge_count.size(); i++) {
-		//cout << "(" << edge_count[i].id << ", " << edge_count[i].count << ") ";
-		heur->add(edge_count[i].id);
-	}
-
-	cout << "Heuristic fitness is " << heur->fitness << endl;
+	int fclose (FILE * stream);
 }
+
+/* minKey finds the vertex with minimum key value, from 
+ * the set of vertices NOT YET included in MST 
+ */ 
+int minKey(int key[], bool mstSet[]) 
+{ 
+	// Initialize min value 
+	int min = INT_MAX, 
+			  min_index; 
+
+	for (int v = 0; v < VERT_NO; v++)
+	{ 
+		if (mstSet[v] == false && key[v] < min) 
+			min = key[v], min_index = v; 
+	}
+	
+	return min_index; 
+} 
+
+/* A utility function to print the constructed MST stored in parent[]*/ 
+void printMST(int parent[], int graph[VERT_NO][VERT_NO]) 
+{ 
+	cout<<"Edge \tWeight\n"; 
+	for (int i = 1; i < VERT_NO; i++) 
+		cout << setw(2) << parent[i]<< " - " 
+		<< setw(2) << i << " " 
+		<< setw(2) << graph[i][parent[i]] <<" \n"; 
+} 
+
+/* Function to construct and print MST for a graph represented using 
+adjacency matrix representation */
+void primMST(int graph[VERT_NO][VERT_NO]) 
+{ 
+	int parent[VERT_NO]; 	 // Array to store constructed MST 
+	int key[VERT_NO]; 	 // Key values used to pick minimum weight edge in cut 
+	bool mstSet[VERT_NO];  // To represent set of vertices not yet included in MST 
+
+	// Initialize all keys as INFINITE 
+	for (int i = 0; i < VERT_NO; i++) {key[i] = INT_MAX, mstSet[i] = false;} 
+
+	// Always include first 1st vertex in MST. 
+	// Make key 0 so that this vertex is picked as first vertex. 
+	key[0] = 0; 
+	parent[0] = -1; // First node is always root of MST 
+
+	// The MST will have V vertices 
+	for (int count = 0; count < VERT_NO - 1; count++) 
+	{ 
+		// Pick the minimum key vertex from the set of vertices not yet included in MST 
+		int u = minKey(key, mstSet); 
+		mstSet[u] = true;	// Add the picked vertex to the MST Set 
+
+		/* Update key value and parent index of the adjacent vertices of
+		 * the picked vertex. Consider only those vertices which are not 
+		 * yet included in MST */ 
+		for (int v = 0; v < VERT_NO; v++){
+			// graph[u][v] is non zero only for adjacent vertices of m 
+			// mstSet[v] is false for vertices not yet included in MST 
+			// Update the key only if graph[u][v] is smaller than key[v] 
+			if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) 
+				parent[v] = u, key[v] = graph[u][v];
+		} 
+	} 
+
+	// print the constructed MST 
+	printMST(parent, graph); 
+} 
+
+int main(int argc, char **argv) 
+{ 
+	srand(time(NULL));
+	startup();
+	//sscanf(argv[1], "%d", degree);
+
+	primMST(weight); 
+
+	return 0; 
+} 
+
